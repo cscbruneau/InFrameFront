631fe6c4ceaf548fa6b940e4b345abf36b959a8f
cscbruneau <cscbruneau@gmail.com>
2020-01-02 11:54:08 +0100

    premier commit
    ajout du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { GenericInputComponent } from './generic-input/generic-input.component';
+import { GenericFormComponent } from './generic-form/generic-form.component';
+import { FormConfigurationService } from './services/form-configuration.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    GenericInputComponent,
+    GenericFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [FormConfigurationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/formulaire.model.ts
@@ -0,0 +1,11 @@
+export class Formulaire {
+
+    constructor(
+        public id: number, // identifiant du formulaire
+        public titre: string, // nom du formulaire
+        public nombreColonnes: number,
+        public configurationLabel: number
+    )
+    {}
+
+}
